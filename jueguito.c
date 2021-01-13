2baa1155c953a740eb7dd7a35b1b0d6d41e601d5
syx <syx@advark.localdomain>
2021-01-13 15:26:23 -0300

    Se mueve y tiene fondo

--- jueguito.c
+++ jueguito.c
@@ -39,6 +39,18 @@ int handleEvent(SDL_Event event) {
 	}
 }
 
+void fillBg(int wWidth, int wHeight, SDL_Texture* texture, SDL_Renderer* renderer) {
+	SDL_Rect dstrect = { .x = 0, .y = 0, .w = 32, .h = 32};
+
+	for (int i = 0; i < wWidth; i += 32) {
+		for (int j = 0; j < wHeight; j += 32) {
+			dstrect.x = i;
+			dstrect.y = j;
+			SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+		}
+	}
+}
+
 int main (int argc, char* argv[]) {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
@@ -46,25 +58,24 @@ int main (int argc, char* argv[]) {
 	SDL_CreateWindowAndRenderer(10, 10, SDL_WINDOW_RESIZABLE, &window, &renderer);
 	
 	int wWidth, wHeight;
+	int animationPos = 0;
+	float animationMov = 0.25;
 
-	square.rect.x = 500;
-	square.rect.y = 500;
-	square.rect.w = 100;
-	square.rect.h = 100;
+	square.rect.x = 0;
+	square.rect.y = 0;
+	square.rect.w = 64;
+	square.rect.h = 64;
 
-	square.texture = IMG_LoadTexture(renderer, "drawing.svg");
+	SDL_Texture* bgTexture = IMG_LoadTexture(renderer, "grassTiles.png");
+	square.texture = IMG_LoadTexture(renderer, "ojo.png");
 
-	SDL_Rect collisionBox = { .x = 600, .y = 600, .w = 40, .h = 40 };
-
-	int srcY[6] = {0, 188, 376, 564, 376, 188};
-	int gifFrame = 0;
-	int frameCounter = 0;
+	SDL_Rect collisionBox = { .x = 0x200, .y = 0x200, .w = 64, .h = 64 };
+	SDL_RendererFlip eyeFlip;
 
 	while (handleEvent(event)) {
         SDL_PollEvent(&event);
 
-        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
-        SDL_RenderClear(renderer);
+		fillBg(wWidth, wHeight, bgTexture, renderer);
 
 		SDL_GetWindowSize(window, &wWidth, &wHeight);
 
@@ -74,39 +85,54 @@ int main (int argc, char* argv[]) {
 		SDL_Rect possibleMovement =  square.rect;
 		
 		if (square.up && square.rect.y - 4 >= 0) {
-		//	possibleMovement.y -= 4;
-		//	if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
+			possibleMovement.y -= 4;
+			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
 				square.rect.y -= 4;
 		}
 
 		if (square.down && square.rect.y + 4 <= wHeight - square.rect.h) {
-		//	possibleMovement.y += 4;
-		//	if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
+			possibleMovement.y += 4;
+			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
 				square.rect.y += 4;
 		}
 
 		if (square.left && square.rect.x - 4 >= 0) {
-		//	possibleMovement.x -= 4;
-		//	if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
+			possibleMovement.x -= 4;
+			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
 				square.rect.x -= 4;
 		}
 
 		if (square.right && square.rect.x + 4 <= wWidth - square.rect.w) {
-		//	possibleMovement.x += 4;
-		//	if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
+			possibleMovement.x += 4;
+			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
 				square.rect.x += 4;
 		}
 
-		SDL_Rect srcrect = { 0, srcY[gifFrame], 174, 159 };
-		SDL_RenderCopy(renderer, square.texture, &srcrect, &square.rect);
+
+		if (square.right)
+			eyeFlip = 0;
+		if (square.left)
+			eyeFlip = SDL_FLIP_HORIZONTAL;
+
+		if (square.left | square.right | animationPos) {
+			if (!(square.left | square.right) || animationPos <= -16) {
+				animationMov = 1;
+			} else if (animationPos >= 0) {
+				animationMov = -1;
+			}
+			animationPos += animationMov;
+		}
+
+		SDL_Rect srcrect = { .x = 0, .y = 0, .w = 16, .h = 16};
+		SDL_Rect dstrect = { .x = square.rect.x, .y = square.rect.y + animationPos, .w = square.rect.w, .h = square.rect.h };
+		SDL_RenderCopyEx(renderer, square.texture, &srcrect, &dstrect, 0, NULL, eyeFlip);
 
         SDL_RenderPresent(renderer);
 		SDL_Delay(16);
 
-		frameCounter = frameCounter == 59 ? 0 : frameCounter + 1;
-		gifFrame = frameCounter / 5;
     }
 
+	SDL_DestroyTexture(bgTexture);
 	SDL_DestroyTexture(square.texture);
 	SDL_DestroyRenderer(renderer);
 	SDL_Quit();
