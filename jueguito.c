f1bed00319d10031a1ccb77de1d03a2b830182c7
syx <syx@advark.localdomain>
2021-01-14 12:52:19 -0300

    un poquito de orden

--- jueguito.c
+++ jueguito.c
@@ -4,35 +4,49 @@
 #define PRESSED  1
 #define RELEASED 0
 
-struct Sprite {
-	SDL_Rect rect;
+struct Personaje {
+	SDL_Rect hitbox;
 	int up, down, left, right;
+	int direction;
+	SDL_Rect textureRect;
 	SDL_Texture* texture;
-} square;
+	int bobbingPos, bobbingMov;
+};
 
-void handleKey(int state, SDL_KeyboardEvent *keyEvent) {
-	if (keyEvent->keysym.scancode == SDL_SCANCODE_W)
-		square.up    = state ? 1 : 0;
-
-	if (keyEvent->keysym.scancode == SDL_SCANCODE_S)
-		square.down  = state ? 1 : 0;
-
-	if (keyEvent->keysym.scancode == SDL_SCANCODE_A)
-		square.left  = state ? 1 : 0;
-
-	if (keyEvent->keysym.scancode == SDL_SCANCODE_D)
-		square.right = state ? 1 : 0;
+void textureBank(int load, SDL_Renderer* renderer, int length, char** files, SDL_Texture** textures) {
+	if (load)
+		for (int i = 0; i < length; i++)
+			textures[i] = IMG_LoadTexture(renderer, files[i]);
 }
 
-int handleEvent(SDL_Event event) {
+void freeTextures(int length, SDL_Texture** textures) {
+	for (int i = 0; i < length; i++) 
+		SDL_DestroyTexture(textures[i]);
+}
+
+void handleKey(int state, SDL_KeyboardEvent *keyEvent, struct Personaje* jugador) {
+	if (keyEvent->keysym.scancode == SDL_SCANCODE_W)
+		jugador->up    = state ? 1 : 0;
+
+	if (keyEvent->keysym.scancode == SDL_SCANCODE_S)
+		jugador->down  = state ? 1 : 0;
+
+	if (keyEvent->keysym.scancode == SDL_SCANCODE_A)
+		jugador->left  = state ? 1 : 0;
+
+	if (keyEvent->keysym.scancode == SDL_SCANCODE_D)
+		jugador->right = state ? 1 : 0;
+}
+
+int handleEvent(SDL_Event event, struct Personaje* jugador) {
 	switch (event.type) {
 		case SDL_QUIT:
 			return 0;
 		case SDL_KEYDOWN:
-			handleKey(PRESSED, &event.key);
+			handleKey(PRESSED, &event.key, jugador);
 			return 1;
 		case SDL_KEYUP:
-			handleKey(RELEASED, &event.key);
+			handleKey(RELEASED, &event.key, jugador);
 			return 1;
 		default:
 			return 1;
@@ -51,90 +65,83 @@ void fillBg(int wWidth, int wHeight, SDL_Texture* texture, SDL_Renderer* rendere
 	}
 }
 
-int main (int argc, char* argv[]) {
+void movPersonaje(int wWidth, int wHeight, struct Personaje* pj) {
+	SDL_Rect possibleMovement =  pj->hitbox;
+	
+	if (pj->right)
+		pj->direction = 0;
+	if (pj->left)
+		pj->direction = 1;
+
+	if (pj->up && pj->hitbox.y - 4 >= 0)
+		pj->hitbox.y -= 4;
+
+	if (pj->down && pj->hitbox.y + 4 <= wHeight - pj->hitbox.h)
+		pj->hitbox.y += 4;
+
+	if (pj->left && pj->hitbox.x - 4 >= 0)
+		pj->hitbox.x -= 4;
+
+	if (pj->right && pj->hitbox.x + 4 <= wWidth - pj->hitbox.w)
+		pj->hitbox.x += 4;
+}
+
+void dibujarPersonaje(struct Personaje* pj, SDL_Renderer* renderer) {
+	SDL_RendererFlip pjFlip = pj->direction ? SDL_FLIP_HORIZONTAL : 0;
+
+	if (pj->left | pj->right | pj->bobbingPos) {
+		if (!(pj->left | pj->right) || pj->bobbingPos <= -16) {
+			pj->bobbingMov = 1;
+		} else if (pj->bobbingPos >= 0) {
+			pj->bobbingMov = -1;
+		}
+		pj->bobbingPos += pj->bobbingMov;
+	}
+
+	pj->textureRect.x = pj->hitbox.x - 8;
+	pj->textureRect.y = pj->hitbox.y - 8 + pj->bobbingPos;
+	SDL_RenderCopyEx(renderer, pj->texture, NULL, &pj->textureRect, 0, NULL, pjFlip);
+}
+
+int main (int argc, char *argv[]) {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-	SDL_Event event;
+	SDL_Event event = {};
 	SDL_CreateWindowAndRenderer(10, 10, SDL_WINDOW_RESIZABLE, &window, &renderer);
 	
 	int wWidth, wHeight;
-	int animationPos = 0;
-	float animationMov = 0.25;
 
-	square.rect.x = 0;
-	square.rect.y = 0;
-	square.rect.w = 64;
-	square.rect.h = 64;
+	char* files[2] = {"ojo.png", "grassTiles.png"};
+	SDL_Texture* textures[2];
+	textureBank(1, renderer, 2, files, textures);
 
-	SDL_Texture* bgTexture = IMG_LoadTexture(renderer, "grassTiles.png");
-	square.texture = IMG_LoadTexture(renderer, "ojo.png");
+	struct Personaje jugador = {
+		.hitbox = { .x = 8, .y = 8, .w = 48, .h = 48 },
+		.up = 0, .down = 0, .left = 0, .right = 0,
+		.direction = 0,
+		.textureRect = { .x = 0, .y = 0, .w = 64, .h = 64 },
+		.texture = textures[0],
+		.bobbingPos = 0, .bobbingMov = 1
+	};
 
-	SDL_Rect collisionBox = { .x = 0x200, .y = 0x200, .w = 64, .h = 64 };
-	SDL_RendererFlip eyeFlip;
+	SDL_Texture* bgTexture = textures[1];
 
-	while (handleEvent(event)) {
+	while (handleEvent(event, &jugador)) {
         SDL_PollEvent(&event);
 
-		fillBg(wWidth, wHeight, bgTexture, renderer);
-
 		SDL_GetWindowSize(window, &wWidth, &wHeight);
-
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
-		SDL_RenderDrawRect(renderer, &collisionBox);
-
-		SDL_Rect possibleMovement =  square.rect;
 		
-		if (square.up && square.rect.y - 4 >= 0) {
-			possibleMovement.y -= 4;
-			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
-				square.rect.y -= 4;
-		}
-
-		if (square.down && square.rect.y + 4 <= wHeight - square.rect.h) {
-			possibleMovement.y += 4;
-			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
-				square.rect.y += 4;
-		}
-
-		if (square.left && square.rect.x - 4 >= 0) {
-			possibleMovement.x -= 4;
-			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
-				square.rect.x -= 4;
-		}
-
-		if (square.right && square.rect.x + 4 <= wWidth - square.rect.w) {
-			possibleMovement.x += 4;
-			if (!SDL_HasIntersection(&possibleMovement, &collisionBox))
-				square.rect.x += 4;
-		}
-
-
-		if (square.right)
-			eyeFlip = 0;
-		if (square.left)
-			eyeFlip = SDL_FLIP_HORIZONTAL;
-
-		if (square.left | square.right | animationPos) {
-			if (!(square.left | square.right) || animationPos <= -16) {
-				animationMov = 1;
-			} else if (animationPos >= 0) {
-				animationMov = -1;
-			}
-			animationPos += animationMov;
-		}
-
-		SDL_Rect srcrect = { .x = 0, .y = 0, .w = 16, .h = 16};
-		SDL_Rect dstrect = { .x = square.rect.x, .y = square.rect.y + animationPos, .w = square.rect.w, .h = square.rect.h };
-		SDL_RenderCopyEx(renderer, square.texture, &srcrect, &dstrect, 0, NULL, eyeFlip);
+		fillBg(wWidth, wHeight, bgTexture, renderer);
+		movPersonaje(wWidth, wHeight, &jugador);
+		dibujarPersonaje(&jugador, renderer);
 
         SDL_RenderPresent(renderer);
 		SDL_Delay(16);
-
     }
 
-	SDL_DestroyTexture(bgTexture);
-	SDL_DestroyTexture(square.texture);
+	freeTextures(2, textures);
 	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 	return 0;
 }
